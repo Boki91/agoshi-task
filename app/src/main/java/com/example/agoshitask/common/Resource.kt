a42a9b7d469b8f16a524414e19a079582a8e8594
Boban Sijuk <sijukboban@gmail.com>
2023-12-04 23:08:31 +0100

    Implement Resource Class
    Define a class to represent different states

---
+++ app/src/main/java/com/example/agoshitask/common/Resource.kt
@@ -0,0 +1,7 @@
+package com.example.agoshitask.common
+
+sealed class Resource<T>(val data: T? = null) {
+    class Success<T>(data: T?) : Resource<T>(data)
+    class NoInternet<T>(data: T?) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+}
